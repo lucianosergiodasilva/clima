c973cfcca56ad21e0bc8a06126e6a99ee0cf9f5a
lucianosergiodasilva <lucianomirabolant@gmail.com>
2026-05-15 17:21:00 -0300

    Envio dos primeiros arquivos.

---
+++ script.js
@@ -0,0 +1,30 @@
+import { KEY_API_OPENWEATHER } from "./config.js";
+
+console.log(KEY_API_OPENWEATHER);
+
+// Obtém o valor do input
+function obterInputValue() {}
+
+// Evento que busca a cidade
+function buscarCidadeSubmit() {}
+
+// Acessa API
+function getApiOpenWeather() {}
+
+// Exibe um alerta na tela
+function mostrarMensagem(mensagem) {}
+
+// Renderiza os dados da API na tela
+function renderizarDados(json) {}
+
+/*
+
+key api:
+035b2a34c684eba5b4fedaa6caea0eb0
+
+    [ ] Acessar api
+    [ ] Obter a localização (cidade) do dispositivo.
+    [ ] Converter valores decimais para uma casa decimal.
+    [ ] Transformar "." em ","
+
+*/
