89ab7165e8f51ca8aa5b3589d96b129588784f54
lucianosergiodasilva <lucianomirabolant@gmail.com>
2026-05-15 23:54:53 -0300

    já peguei alguns dados da api.

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 import { KEY_API_OPENWEATHER } from "./config.js";
 
 const cidade = "Rio de Janeiro";
-const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${KEY_API_OPENWEATHER}`;
 
-const respostaApi = fetch(urlApi)
-  .then((res) => res.json())
-  .then((dados) =>
-    console.log({
-      temp: dados.main.temp,
-      max: dados.main.temp_max,
-      min: dados.main.temp_min,
-      umidity: dados.main.humidity,
-      name: dados.name,
-      dt: dados,
-    }),
-  )
-  .catch((err) => console.error("Erro: ", err));
+// Acessa a api e obtém os dados
+const getApiOpenWeather = async () => {
+  const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${KEY_API_OPENWEATHER}&units=metric&lang=pt_br`;
+  // Controle de erro
+  try {
+    // Acessa a api
+    const resposta = await fetch(urlApi);
+    // Verifica e informa o status code
+    if (!resposta.ok) {
+      throw new Error(`HTTP error! status: ${resposta.status}`);
+    }
+    // Obtém os dados da api
+    const dados = await resposta.json();
 
-console.log(respostaApi);
+    // Renderizar dados
+    renderizarDados(dados);
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+};
+
+// const respostaApi = fetch(urlApi)
+//   .then((res) => res.json())
+//   .then((dados) =>
+//     console.log({
+//       temperatura: dados.main.temp,
+//       temperaturaMax: dados.main.temp_max,
+//       temperaturaMin: dados.main.temp_min,
+//       umidade: dados.main.humidity,
+//       cidade: dados.name,
+//       sensacaoTermica: dados.main.feels_like,
+//       vento: dados.wind.speed,
+//       clima: {
+//         tempo: dados.weather[0].main,
+//         descricao: dados.weather[0].description,
+//       },
+//     }),
+//   )
+//   .catch((err) => console.error("Erro: ", err));
+
+console.log(getApiOpenWeather());
 
 // Obtém o valor do input
 function obterInputValue() {}
@@ -25,20 +50,48 @@ function obterInputValue() {}
 // Evento que busca a cidade
 function buscarCidadeSubmit() {}
 
-// Acessa API
-function getApiOpenWeather() {}
-
 // Exibe um alerta na tela
 function mostrarMensagem(mensagem) {}
 
 // Renderiza os dados da API na tela
-function renderizarDados(json) {}
+function renderizarDados(dados) {
+  // Dados da api
+  const dadosApi = {
+    temperatura: dados.main.temp,
+    temperaturaMax: dados.main.temp_max,
+    temperaturaMin: dados.main.temp_min,
+    umidade: dados.main.humidity,
+    cidade: dados.name,
+    pais: dados.sys.country,
+    sensacaoTermica: dados.main.feels_like,
+    vento: dados.wind.speed,
+    descricao: dados.weather[0].description,
+    icone: dados.weather[0].icon,
+  };
+
+  // Elementos HTML
+  document.querySelector(".cidade h1").innerHTML = `<i class="fa-solid fa-location-dot"></i> ${dadosApi.cidade}, ${dadosApi.pais}`;
+  document.querySelector(".icone-maior").src = `https://openweathermap.org${dadosApi.icone}@2x.png`;
+  document.querySelector(".temperatura-destaque h2").innerHTML = `${Math.trunc(dadosApi.temperatura)} ºC`;
+  document.querySelector(".temperatura-destaque p").innerHTML = `${dadosApi.descricao}`;
+  document.querySelector(".temperatura__maxima div h3").innerHTML = `${Math.trunc(dadosApi.temperaturaMax)} ºC`;
+  document.querySelector(".temperatura__minima div h3").innerHTML = `${Math.trunc(dadosApi.temperaturaMin)} ºC`;
+  document.querySelector(".umidade h3").innerHTML = `${dadosApi.umidade}%`;
+  document.querySelector(".vento h3").innerHTML = `${dadosApi.vento}km/h`;
+  document.querySelector(".nuvem h4").innerHTML = `${Math.trunc(dadosApi.sensacaoTermica)} ºC`;
+}
 
 /*
 
-    [ ] Acessar api
+  NÃO ESTOU CONSEGUINDO PEGAR O ÍCONE DA API
+
+
+    [x] Acessar api
     [ ] Obter a localização (cidade) do dispositivo.
-    [ ] Converter valores decimais para uma casa decimal.
-    [ ] Transformar "." em ","
+    [x] Converter valores decimais para uma casa decimal.
+    [ ] Pegar o ícone da api
+    [ ] Criar uma verificação para a dica, baseado no horário e clima
+    [ ] Inserir as temperaturas para diferentes horarios do dia (6h, 18h e 00h)
+
 
 */
