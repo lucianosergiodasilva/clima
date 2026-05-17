1c59c519afc85df04e99cc1e50ce88416a984135
lucianosergiodasilva <lucianomirabolant@gmail.com>
2026-05-17 20:40:47 -0300

    Projeto finalizado por hoje

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-import { KEY_API_OPENWEATHER } from "./config.js";
+import { KEY_API_OPENWEATHER, KEY_API_UNSPLASH, URL_API_OPENWEATHER } from "./config.js";
 
-const cidade = "Rio de Janeiro";
-
+// INSERE UMA FOTO DE FUNDO
 // Acessa a api e obtém os dados
-const getApiOpenWeather = async () => {
-  const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${KEY_API_OPENWEATHER}&units=metric&lang=pt_br`;
+async function getApiUnsplash(cidade) {
+  const urlApi = `https://api.unsplash.com/search/photos?page=1&query=${encodeURI(cidade)}&client_id=${KEY_API_UNSPLASH}`;
+  // Controle de erro
+  try {
+    // Acessa a api
+    const resposta = await fetch(urlApi);
+    // Verifica e informa o status code
+    if (!resposta.ok) {
+      throw new Error(`HTTP error! status: ${resposta.status}`);
+    }
+    // Obtém os dados da api
+    const dados = await resposta.json();
+
+    // Renderizar dados
+    const url = dados.results[0].urls.regular;
+    document.querySelector(".fundo").style.backgroundImage = `url('${url}')`;
+    document.querySelector(".fundo").style.backgroundSize = `cover`;
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+}
+
+// Acessa a api Open Weather quando clica em buscar
+async function getApiOpenWeather(cidade) {
+  const urlApi = `${URL_API_OPENWEATHER}?q=${encodeURI(cidade)}&appid=${KEY_API_OPENWEATHER}&units=metric&lang=pt_br`;
   // Controle de erro
   try {
     // Acessa a api
@@ -21,37 +43,62 @@ const getApiOpenWeather = async () => {
   } catch (error) {
     console.error("Erro ao obter dados:", error);
   }
-};
+}
 
-// const respostaApi = fetch(urlApi)
-//   .then((res) => res.json())
-//   .then((dados) =>
-//     console.log({
-//       temperatura: dados.main.temp,
-//       temperaturaMax: dados.main.temp_max,
-//       temperaturaMin: dados.main.temp_min,
-//       umidade: dados.main.humidity,
-//       cidade: dados.name,
-//       sensacaoTermica: dados.main.feels_like,
-//       vento: dados.wind.speed,
-//       clima: {
-//         tempo: dados.weather[0].main,
-//         descricao: dados.weather[0].description,
-//       },
-//     }),
-//   )
-//   .catch((err) => console.error("Erro: ", err));
+// Acessa a api Open Weather quando a página carrega
+// Pega a latitude e longitudo do navegador
+async function getApiReverseGeocoding(lat, lon) {
+  const urlApi = `${URL_API_OPENWEATHER}?lat=${lat}&lon=${lon}&appid=${KEY_API_OPENWEATHER}&units=metric&lang=pt_br`;
+  // Controle de erro
+  try {
+    // Acessa a api
+    const resposta = await fetch(urlApi);
+    // Verifica e informa o status code
+    if (!resposta.ok) {
+      throw new Error(`HTTP error! status: ${resposta.status}`);
+    }
+    // Obtém os dados da api
+    const dados = await resposta.json();
 
-console.log(getApiOpenWeather());
+    // Renderizar dados
+    renderizarDados(dados);
+    getApiUnsplash(dados.name);
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+}
 
-// Obtém o valor do input
-function obterInputValue() {}
+// Obter localização do navegador
+function getLocationCurrent() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      getApiReverseGeocoding(lat, lon);
+    });
+  } else {
+    console.log("Alguma coisa não deu certo...");
+  }
+}
 
 // Evento que busca a cidade
-function buscarCidadeSubmit() {}
+function buscarCidadeSubmit() {
+  const cidade = getInputValue();
+  getApiUnsplash(cidade);
+  getApiOpenWeather(cidade);
+}
 
-// Exibe um alerta na tela
-function mostrarMensagem(mensagem) {}
+// Obtém o valor do input
+function getInputValue() {
+  const formInputValue = document.querySelector(".form__input").value;
+  return formInputValue;
+}
+
+// Exibe uma mensagem na tela
+function mostrarMensagem(valor) {
+  const resultado = valor >= 25 ? "Uma praia cairia bem." : valor >= 20 ? "Está esquentando, não acha?" : valor >= 15 ? "Está bem agradável." : valor >= 10 ? "Que geeelo! Que frio!" : valor >= 5 ? "Extremamente frio" : "Como está a temperatura?";
+  document.querySelector(".dica h4").innerText = resultado;
+}
 
 // Renderiza os dados da API na tela
 function renderizarDados(dados) {
@@ -71,27 +118,29 @@ function renderizarDados(dados) {
 
   // Elementos HTML
   document.querySelector(".cidade h1").innerHTML = `<i class="fa-solid fa-location-dot"></i> ${dadosApi.cidade}, ${dadosApi.pais}`;
-  document.querySelector(".icone-maior").src = `https://openweathermap.org${dadosApi.icone}@2x.png`;
+  document.querySelector(".icone-maior").src = `https://openweathermap.org/payload/api/media/file/${dadosApi.icone}.png`;
   document.querySelector(".temperatura-destaque h2").innerHTML = `${Math.trunc(dadosApi.temperatura)} ºC`;
   document.querySelector(".temperatura-destaque p").innerHTML = `${dadosApi.descricao}`;
   document.querySelector(".temperatura__maxima div h3").innerHTML = `${Math.trunc(dadosApi.temperaturaMax)} ºC`;
   document.querySelector(".temperatura__minima div h3").innerHTML = `${Math.trunc(dadosApi.temperaturaMin)} ºC`;
-  document.querySelector(".umidade h3").innerHTML = `${dadosApi.umidade}%`;
-  document.querySelector(".vento h3").innerHTML = `${dadosApi.vento}km/h`;
-  document.querySelector(".nuvem h4").innerHTML = `${Math.trunc(dadosApi.sensacaoTermica)} ºC`;
+  document.querySelector(".umidade .titulo").innerHTML = `${dadosApi.umidade} %`;
+  document.querySelector(".vento .titulo").innerHTML = `${dadosApi.vento} km/h`;
+  document.querySelector(".sensacao .titulo").innerHTML = `${Math.trunc(dadosApi.sensacaoTermica)} ºC`;
+
+  // Mostrar mensagem na tela
+  mostrarMensagem(Math.trunc(dadosApi.temperatura));
 }
+
+document.addEventListener("DOMContentLoaded", getLocationCurrent);
+document.querySelector(".form__botao").addEventListener("click", buscarCidadeSubmit);
 
 /*
 
-  NÃO ESTOU CONSEGUINDO PEGAR O ÍCONE DA API
-
-
+    [x] A função getApiOpenWeatherLocation nao está funcionando
     [x] Acessar api
-    [ ] Obter a localização (cidade) do dispositivo.
+    [x] Obter a localização (cidade) do dispositivo.
     [x] Converter valores decimais para uma casa decimal.
-    [ ] Pegar o ícone da api
-    [ ] Criar uma verificação para a dica, baseado no horário e clima
-    [ ] Inserir as temperaturas para diferentes horarios do dia (6h, 18h e 00h)
-
+    [x] Pegar o ícone da api
+    [x] Criar uma verificação para a dica, baseado no horário e clima
 
 */
